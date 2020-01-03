81abb6742cf3ee68660528be28a6b53b5d1e58f2
Boldeusz <mnpower@poczta.onet.pl>
2020-01-03 15:01:56 +0100

    update

--- src/com/example/test/AnotherThread.java
+++ src/com/example/test/AnotherThread.java
@@ -1,9 +1,11 @@
 package com.example.test;
 
+import static com.example.test.ThreadColor.ANSI_BLUE;
+
 public class AnotherThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Hello from AnotherThread!!!");
+        System.out.println(ANSI_BLUE + "Hello from AnotherThread!!!");
     }
 }
--- src/com/example/test/Main.java
+++ src/com/example/test/Main.java
@@ -1,16 +1,19 @@
 package com.example.test;
 
+import static com.example.test.ThreadColor.ANSI_GREEN;
+import static com.example.test.ThreadColor.ANSI_PURPLE;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from the main thread.");
+        System.out.println(ANSI_PURPLE + "Hello from the main thread.");
 
         Thread anotherThread = new AnotherThread();
         anotherThread.start();
 
         new Thread() {
             public void run() {
-                System.out.println("Hello from anonymous class thread");
+                System.out.println(ANSI_GREEN + "Hello from anonymous class thread");
             }
         }.start();
 
